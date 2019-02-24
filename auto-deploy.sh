3a5a9aa4e5bc7e6e579040c21639d347dcbde28d
fun4wut <nihouze@163.com>
2019-02-24 15:51:41 +0800

    这才是最终形态ORZ

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-ssh  root@120.78.172.241 "source ~/.zshrc;cd ~/blog/source;hexo g;hexo d"
+ssh  root@120.78.172.241 "source ~/.zshrc;cd ~/blog/source;git pull origin hexo;hexo g;hexo d"
